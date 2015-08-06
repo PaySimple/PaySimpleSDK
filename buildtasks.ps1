211467431d839ba5f4454d4c6293482bec7c1ba2
Scott Lance <slance@cmnhospitals.org>
2015-08-06 12:47:43 -0600

    Updated files for v1.0.1

--- buildtasks.ps1
+++ buildtasks.ps1
@@ -9,7 +9,7 @@ properties {
 	$xunit_path = "$src_directory\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"	
 	$nuget_path = "$src_directory\.nuget\nuget.exe"
 
-	$version = "1.0.0"
+	$version = "1.0.1"
 	$preRelease = ""
 }
 
