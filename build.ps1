df540f0e869b19e52f00bd41d582a1583b5e127a
Scott Lance <slance@cmnhospitals.org>
2015-12-04 13:08:51 -0700

    Updated build script to use psake 4.4.2

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@ gci .\src -Recurse "packages.config" |% {
 	.\src\.nuget\nuget.exe i $_.FullName -o .\src\packages
 }
 
-Import-Module .\src\packages\psake.4.4.1\tools\psake.psm1
+Import-Module .\src\packages\psake.4.4.2\tools\psake.psm1
 Invoke-Psake .\buildtasks.ps1 -framework "4.0x64" 
 Remove-Module psake
